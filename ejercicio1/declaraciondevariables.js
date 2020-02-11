c1f251842306787a60917d931ae19eedda5b3f9e
505 <505@academicos.cesde.edu.co>
2020-02-10 20:45:33 -0500

    Ejemplo de node api

---
+++ ejercicio1/declaraciondevariables.js
@@ -0,0 +1,15 @@
+let nombre ="camilo";
+let nombre = "sara";
+console.log(nombre);
+
+let costo = 5;
+let comida = "";
+if(costo > 3){
+     comida ="galletas";
+}
+console.log("la comida"+ comida);
+
+
+function comer(cosas){
+    var cosas = "cosas";
+}
